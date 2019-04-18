cb4befbcdf488e839048b0177a829534a9d27f04
phongpadid <bay@phongpadids-MBP.gateway>
2019-04-18 17:22:57 +1000

    added hamburger stack menu

---
+++ BIC/mainViewController.h
@@ -0,0 +1,30 @@
+//
+//  mainViewController.h
+//  BIC
+//
+//  Created by phongpadid on 4/18/19.
+//  Copyright © 2019 phongpadid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface mainViewController : UIViewController {
+    
+ //  IBOutlet UIView *transV;
+   //IBOutlet UIView *sidePanel;
+   //IBOutlet UIButton *menuBtn;
+    
+    
+}
+
+@property (weak, nonatomic) IBOutlet UIButton *menuBtn;
+@property (weak, nonatomic) IBOutlet UIView *transV;
+@property (weak, nonatomic) IBOutlet UIView *sidePanel;
+
+- (IBAction)menuBtnn:(UIButton *)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BIC/mainViewController.m
@@ -0,0 +1,102 @@
+//
+//  mainViewController.m
+//  BIC
+//
+//  Created by phongpadid on 4/18/19.
+//  Copyright © 2019 phongpadid. All rights reserved.
+//
+
+#import "mainViewController.h"
+
+@interface mainViewController ()
+
+@end
+
+@implementation mainViewController
+
+@synthesize sidePanel, menuBtn, transV;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // hide the navigation bar that inherit from landing page !
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+
+    
+    // use the Gesture recognizer for hidding the sidebar !
+    UITapGestureRecognizer *tapper = [[ UITapGestureRecognizer alloc ] initWithTarget: self action: @selector(hideSidePanel:)];
+    
+    tapper.numberOfTapsRequired = 1 ;
+    [transV addGestureRecognizer: tapper ];
+}
+
+-(void)hideSidePanel: (UIGestureRecognizer *) gesture {
+    
+    if ( gesture.state == UIGestureRecognizerStateEnded ) {
+        
+        
+            
+            [ transV setHidden: YES ];
+            
+            [ UIView transitionWithView: sidePanel  duration: 0.2 options: UIViewAnimationOptionCurveEaseIn animations:^{
+                
+                CGRect frame = self->sidePanel.frame;
+                frame.origin.x = -self->sidePanel.frame.size.width ;
+                self->sidePanel.frame = frame;
+                
+            } completion: nil ];
+            
+            
+        }
+    
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    
+    if ( sender == menuBtn ) {
+        
+        [ transV setHidden: NO ];
+        
+        [ UIView transitionWithView: sidePanel  duration: 0.2 options: UIViewAnimationOptionCurveEaseIn animations:^{
+            
+            CGRect frame = self->sidePanel.frame;
+            frame.origin.x = 0 ;
+            self->sidePanel.frame = frame;
+            
+        } completion: nil ];
+            
+        
+    }
+        
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)menuBtnn:(UIButton *)sender {
+    
+    if ( sender == menuBtn ) {
+        
+        [ transV setHidden: NO ];
+        
+        [ UIView transitionWithView: sidePanel  duration: 0.2 options: UIViewAnimationOptionCurveEaseIn animations:^{
+            
+            CGRect frame = self->sidePanel.frame;
+            frame.origin.x = 0 ;
+            self->sidePanel.frame = frame;
+            
+        } completion: nil ];
+        
+        
+    }
+}
+@end
